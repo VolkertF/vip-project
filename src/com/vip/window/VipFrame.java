f333fc209fc58e0ce06db2477a9da676eb6efe14
Johannes Licht <johannes.licht@stud.hs-hannover.de>
2015-09-17 11:07:40 +0200

    Added the swing stuff necessary for the file search and comment everything

--- src/com/vip/window/VipFrame.java
+++ src/com/vip/window/VipFrame.java
@@ -3,17 +3,18 @@ package com.vip.window;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 
@@ -28,13 +29,32 @@ public class VipFrame extends JFrame {
 		defaultInsets = new Insets(2, 2, 2, 2);
 		
 		buildPanels();
+		buildExplorerGUI();
+		buildMovieGUI();
+		buildIntelGUI();
 		pack();
-		
 	}
 
+	/**
+	 * The JPanel that represents the Explorer and do file-searching stuff and listing all the videos
+	 * in a JList, so the user is able to select one and play it
+	 */
 	private JPanel jpnlExplorer;
+	
+	/**
+	 * The JPanel that represents the movie been played by the VLC plugin
+	 */
 	private JPanel jpnlMovie;
+	
+	/**
+	 * The JPanel that represents the information section of the program, where intel about the currently selected
+	 * movie can be clicked on so the program do a search for this one, and various information about the title can be viewed.
+	 */
 	private JPanel jpnlIntel;
+	
+	/**
+	 * Standard insets for creating the GridBagLayout
+	 */
 	private Insets defaultInsets;
 
 	/**
@@ -71,6 +91,7 @@ public class VipFrame extends JFrame {
 		jpnlExplorer = new JPanel();
 		jpnlExplorer.setLayout(new FlowLayout());
 		jpnlExplorer.setBorder(BorderFactory.createTitledBorder("Explorer"));
+		jpnlExplorer.setPreferredSize(new Dimension(250, 800));
 		
 		jpnlMovie = new JPanel();
 		jpnlMovie.setLayout(new BorderLayout());
@@ -79,6 +100,7 @@ public class VipFrame extends JFrame {
 		jpnlIntel = new JPanel();
 		jpnlIntel.setLayout(new BorderLayout());
 		jpnlIntel.setBorder(BorderFactory.createTitledBorder("Intel"));
+		jpnlIntel.setPreferredSize(new Dimension(1200, 150));
 
 		getContentPane().setLayout(new GridBagLayout());
 		
@@ -87,28 +109,74 @@ public class VipFrame extends JFrame {
 		addComponent(1, 1, 1, 1, 0.0, 0.0, getContentPane(), jpnlIntel, defaultInsets);
 	}
 	
+	/**
+	 * This JList will fill with the files in all searching directories the program
+	 * overwatches, so basically every movie file found on the harddrive
+	 */
+	private JList<String> jlstFileList;
 	
+	/**
+	 * This JTextField is for entering your private search stuff into a textField to search your library for this
+	 */
+	private JTextField jtfSearch;
 	
+	/**
+	 * This JComboBox is ment for sorting your search by different categories
+	 */
+	private JComboBox<String> jcbSearchCategories;
 	
+	/**
+	 * By pressing this JButton the search by the chosen searching-categories and the
+	 * entered keywords is executed.
+	 */
+	private JButton jbtnSearchExecute;			//Button for executing the search
 	
+	/**
+	 * Create Sub-sub-panels in the explorer panel
+	 */
+	private void buildExplorerGUI() {
+		String[] fileList = {};
+		//Fill the filelist String-Array
+		jlstFileList = new JList<String>(fileList);
+		jlstFileList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		jlstFileList.setSelectedIndex(0);
+		
+		jtfSearch = new JTextField(20);
+		
+		String[] searchCategories = {"By Length, increasing",
+									 "By length, decreasing",
+									 "By rating, increasing",
+									 "By rating, decreasing",};
+		jcbSearchCategories = new JComboBox<String>(searchCategories);
+		jcbSearchCategories.setEditable(false);
+		jcbSearchCategories.setSelectedIndex(0);
+		JScrollPane jspSearchCategories = new JScrollPane(jcbSearchCategories);
+		jspSearchCategories.setPreferredSize(jcbSearchCategories.getSize());
+		
+		jbtnSearchExecute = new JButton("Search");
+		
+		//			 x  y  w  h  wx   wy   cont          comp            insets
+		addComponent(0, 0, 2, 1, 0.0, 0.0, jpnlExplorer, jtfSearch, defaultInsets);
+		addComponent(0, 1, 1, 1, 0.0, 0.0, jpnlExplorer, jcbSearchCategories, defaultInsets);
+		addComponent(1, 1, 1, 1, 0.0, 0.0, jpnlExplorer, jbtnSearchExecute, defaultInsets);
+		addComponent(0, 2, 2, 1, 1.0, 1.0, jpnlExplorer, jlstFileList, defaultInsets);
+	}
 	
+	/**
+	 * Create Sub-sub- components in the movie panel including the JVLC plugin to play movies
+	 * Maybe also a section to control the movie (play, pause, volume up/down, fast forward etc.)
+	 */
+	private void buildMovieGUI() {
+		//Here the VLC Plugin comes to life
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	/**
+	 * Create Sub-sub-components in the intel panel for showing information on the selected video
+	 * file in the explorer section
+	 */
+	private void buildIntelGUI() {
+		//Here some information about the currently selected video file will be stored.
+	}
 	
 	
 	

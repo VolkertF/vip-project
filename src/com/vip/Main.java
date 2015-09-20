5e3897d8a36b80d8756d76c2790a109d7665585d
Johannes Licht <johannes.licht@stud.hs-hannover.de>
2015-09-20 20:23:17 +0200

    Pushing GUI and VLC in the feasibility branch

---
+++ src/com/vip/Main.java
@@ -0,0 +1,12 @@
+package com.vip;
+
+import com.vip.window.VipFrame;
+
+public class Main {
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+		VipFrame f = new VipFrame();
+		f.setVisible(true);
+		return;
+	}
+}

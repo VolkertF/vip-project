883619ed85d41ef770208e33bb8f2d87965f2109
Johannes Licht <johannes.licht@stud.hs-hannover.de>
2015-10-14 20:28:45 +0200

    Added Information Button

--- src/com/vip/window/ContextVideoMenu.java
+++ src/com/vip/window/ContextVideoMenu.java
@@ -1,13 +1,52 @@
 package com.vip.window;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
 
 import com.vip.attributes.Video;
+import com.vip.media.VLC;
 
 @SuppressWarnings("serial")
 public class ContextVideoMenu extends JPopupMenu {
+	private JMenuItem jmiPlay;
+	private JMenuItem jmiInfo;
+	
 	public ContextVideoMenu(Video vid) {
-		this.add(new JMenuItem("Play " + vid.getTitle()));
+		init(vid);
+		addMenuItems();
+	}
+	
+	private void init(Video vid) {
+		final String vidPath = vid.getPath();
+		
+		jmiPlay = new JMenuItem("Play " + vid.getTitle());
+		jmiPlay.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				VLC.loadMedia(vidPath);
+				VLC.toggleMoviePlayback();
+			}
+		});
+		
+		
+		jmiInfo = new JMenuItem("Get Video Information");
+		final String info = "You have selected information about " + vid.getTitle();
+		jmiInfo.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+				JOptionPane.showMessageDialog(getRootPane(), info);
+			}
+		});
+	}
+	
+	private void addMenuItems() {
+		this.add(jmiPlay);
+		this.addSeparator();
+		this.add(jmiInfo);
 	}
 }
